469178575195e327c5e4830f3c5dcec95aea9d8d
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2025-01-17 23:29:48 -0800

    implemented a rough version of conway's game of life

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,65 @@
 use image;
-use num_complex;
 
 fn main() {
-    //! An example of generating julia fractals.
-    let imgx = 800;
-    let imgy = 800;
+    let imgx = 5;
+    let imgy = 5;
 
-    let scalex = 3.0 / imgx as f32;
-    let scaley = 3.0 / imgy as f32;
+    let generations = 5;
 
     // Create a new ImgBuf with width: imgx and height: imgy
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+    let mut imgbuf = image::RgbImage::new(imgx, imgy);
 
-    // Iterate over the coordinates and pixels of the image
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = (0.3 * x as f32) as u8;
-        let b = (0.3 * y as f32) as u8;
-        *pixel = image::Rgb([r, 0, b]);
+    // create initial generation as a blinker and save it
+    for x in 1..=3 {
+        imgbuf.put_pixel(x, 2, image::Rgb([255, 255, 255]))
     }
+    imgbuf.save("output/0001.png").unwrap();
 
-    // A redundant loop to demonstrate reading image data
-    for x in 0..imgx {
-        for y in 0..imgy {
-            let cx = y as f32 * scalex - 1.5;
-            let cy = x as f32 * scaley - 1.5;
+    let mut lastgen = imgbuf;
 
-            let c = num_complex::Complex::new(-0.4, 0.6);
-            let mut z = num_complex::Complex::new(cx, cy);
+    for i in 0..generations {
+        let mut genbuf = image::RgbImage::new(imgx, imgy);
 
-            let mut i = 0;
-            while i < 255 && z.norm() <= 2.0 {
-                z = z * z + c;
-                i += 1;
+        // insert game of life logic here
+        for (x, y, pixel) in genbuf.enumerate_pixels_mut() {
+            let mut live_neighbors = 0;
+            let mut pixel_alive = false;
+            // itterate over neighbors
+            for x_offset in -1i32..=1 {
+                for y_offset in -1i32..=1 {
+                    match lastgen.get_pixel_checked(
+                        (x as i32 + x_offset) as u32,
+                        (y as i32 + y_offset) as u32,
+                    ) {
+                        None => (),
+                        Some(color) => {
+                            if color[0] > 0 {
+                                match (x_offset, y_offset) {
+                                    (0, 0) => pixel_alive = true,
+                                    _ => live_neighbors += 1,
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
-            let pixel = imgbuf.get_pixel_mut(x, y);
-            let image::Rgb(data) = *pixel;
-            *pixel = image::Rgb([data[0], i as u8, data[2]]);
+            match pixel_alive {
+                true => {
+                    if (live_neighbors >= 2) && (live_neighbors <= 3) {
+                        *pixel = image::Rgb([255, 255, 255]);
+                    }
+                }
+                false => {
+                    if live_neighbors == 3 {
+                        *pixel = image::Rgb([255, 255, 255]);
+                    }
+                }
+            }
         }
-    }
 
-    // Save the image as “fractal.png”, the format is deduced from the path
-    imgbuf.save("fractal.png").unwrap();
+        let _ = genbuf.save(format!("output/{:04}.png", i));
+
+        lastgen = genbuf;
+    }
 }
